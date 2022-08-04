fe2d48af7b0c1114525a5595865eaae2ad2234b2
Yusuf Kolawole <quadyusuf@gmail.com>
2022-08-04 14:48:51 +0100

    Finishing 2

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -7,3 +7,4 @@ export TYPEORM_PASSWORD=postgres
 export TYPEORM_DATABASE=postgres
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export check="{{ lookup('env', 'TYPEORM_HOST')}}"
